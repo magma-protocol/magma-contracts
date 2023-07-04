4974bb136763b1d6204b0651cc6815732b9e7f61
Magma <MagMaDev2023@gmail.com>
2023-07-04 22:59:19 +0800

    add summary contract addresses

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # magma-contracts
 This is smart contracts for Magma Protocol.
+
+## Deployments
+
+1. Add Key in `.env` file. It's a private key of the account that will deploy the contracts and should be gitignored.
+2. mantleTestnet `KEY_TESTNET` or mantle `KEY_MAINNET`
+3. add `ETHERSCAN_API_KEY` in `.env` file. It's an API key for etherscan.
+4. `yarn` in root directory
+5. `NETWORK=$NETWORK yarn zx deploy.mjs` where `$NETWORK` is either  `mantleTestnet` or `hardhat` (for local testing)
---
+++ deploy.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env zx
+// import 'zx/globals'
+
+const networks = {
+  hardhat: 'hardhat',
+  mantleTestnet: 'mantleTestnet'
+}
+
+let network = process.env.NETWORK
+console.log(network, 'network')
+if (!network || !networks[network]) {
+  throw new Error(`env NETWORK: ${network}`)
+}
+
+const c = await fs.readJson(`./core/deployments/${network}.json`)
+const p = await fs.readJson(`./periphery/deployments/${network}.json`)
+const m = await fs.readJson(`./masterchef/deployments/${network}.json`)
+const l = await fs.readJson(`./lm-pool/deployments/${network}.json`)
+const la = await fs.readJson(`./launchpad/deployments/${network}.json`)
+
+const addresses = {
+  ...c,
+  ...p,
+  ...m,
+  ...l,
+  ...la,
+}
+
+console.log(chalk.blue('Writing to file...'))
+console.log(chalk.yellow(JSON.stringify(addresses, null, 2)))
+
+fs.writeJson(`./deployments/${network}.json`, addresses, { spaces: 2 })
